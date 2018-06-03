6642b97cad0b5621d4b975c6c71cec6da5e93d13
Amar Prakash Pandey <amar.om1994@gmail.com>
2018-06-03 22:03:11 +0530

    basic programs and data images added

---
+++ Programs/ColorPalette.py
@@ -0,0 +1,35 @@
+import cv2
+import numpy as np
+
+
+def passFunction(x):
+    pass
+
+def main():
+
+    windowName = "OpenCV BGR Color Palette"
+    imageData = np.zeros((512, 512, 3), np.uint8)
+
+    cv2.namedWindow(windowName)
+
+    cv2.createTrackbar('Blue', windowName, 0, 255, passFunction)
+    cv2.createTrackbar('Green', windowName, 0, 255, passFunction)
+    cv2.createTrackbar('Red', windowName, 0, 255, passFunction)
+
+    while(True):
+        cv2.imshow(windowName, imageData)
+
+        if cv2.waitKey(1) == 27:
+            break
+
+        blue = cv2.getTrackbarPos('Blue', windowName)
+        green = cv2.getTrackbarPos('Green', windowName)
+        red = cv2.getTrackbarPos('Red', windowName)
+
+        imageData[:] = [blue, green, red]
+        print(blue, green, red)
+
+    cv2.destroyWindow(windowName)
+
+if __name__ == '__main__':
+    main()
---
+++ Programs/DrawShape.py
@@ -0,0 +1,58 @@
+import cv2
+import numpy as np
+
+
+mode = True
+xi, yi = -1, -1  # type: (int, int)
+drawing  = False
+
+
+windowName = "Drawing Shapes"
+image = np.zeros((512, 800, 3), np.uint8)
+cv2.namedWindow(windowName)
+
+
+def drawShape(event, x, y, flags, params):
+
+    global mode, drawing, xi, yi
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        drawing = True
+        xi, yi = x, y
+
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if drawing == True:
+            if mode == True:
+                cv2.rectangle(image, (xi, yi), (x, y), (0, 255, 0), -1)
+            else:
+                cv2.circle(image, (x, y), 5, (255, 0, 0), -1)
+
+    elif event == cv2.EVENT_LBUTTONUP:
+        drawing = False
+        if mode == True:
+            cv2.rectangle(image, (xi, yi), (x, y), (0, 255, 0), -1)
+        else:
+            cv2.circle(image, (x, y), 5, (255, 0, 0), -1)
+
+
+cv2.setMouseCallback(windowName, drawShape)
+
+
+def main():
+
+    global mode
+
+    while True:
+        preseedKey = cv2.waitKey(1)
+        cv2.imshow(windowName, image)
+
+        if preseedKey == ord('m') or preseedKey == ord('M'):
+            mode = not mode
+        elif preseedKey == 27:
+            break
+
+    cv2.destroyWindow(windowName)
+
+
+if __name__ == '__main__':
+    main()
