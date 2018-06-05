a3c16c45bf191f7e1a08bf20b5f3bd12e946eeb5
Amar Prakash Pandey <amar.om1994@gmail.com>
2018-06-05 23:51:59 +0530

    videocamera.py added

---
+++ models/VideoCamera.py
@@ -0,0 +1,32 @@
+import cv2
+
+
+def main():
+
+    window_name = "Live Video Feed"
+    cv2.namedWindow(window_name)
+
+    capture = cv2.VideoCapture(0)
+
+    capture.set(cv2.CAP_PROP_FRAME_WIDTH, 5000)
+    capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 5000)
+
+    if capture.isOpened():
+        flag, frame = capture.read()
+    else:
+        flag = False
+
+    while flag:
+
+        flag, frame = capture.read()
+
+        cv2.imshow(window_name, frame)
+        if cv2.waitKey(1) == 27:
+            break
+
+    cv2.destroyWindow(window_name)
+    capture.release()
+
+
+if __name__ == '__main__':
+    main()
