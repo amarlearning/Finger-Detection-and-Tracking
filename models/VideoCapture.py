840949f81c842215a229c575113f5645185e710f
Amar Prakash Pandey <amar.om1994@gmail.com>
2018-06-06 00:11:12 +0530

    capture video output

---
+++ models/VideoCapture.py
@@ -0,0 +1,36 @@
+import cv2
+
+
+def main():
+    framerate = 30
+    resolution = (640, 480)
+    window_name = "Live Video Feed"
+    videoCapturePath = "/home/amarpandey/PycharmProjects/OpenCV/Output/Output.avi"
+
+    cv2.namedWindow(window_name)
+    capture = cv2.VideoCapture(0)
+
+    codec = cv2.VideoWriter_fourcc('X', 'V', 'I', 'D')
+
+    videoCapture = cv2.VideoWriter(videoCapturePath, codec, framerate, resolution)
+
+    if capture.isOpened():
+        flag, frame = capture.read()
+    else:
+        flag = False
+
+    while flag:
+
+        flag, frame = capture.read()
+        videoCapture.write(frame)
+        cv2.imshow(window_name, frame)
+        if cv2.waitKey(1) == 27:
+            break
+
+    cv2.destroyWindow(window_name)
+    videoCapture.release()
+    capture.release()
+
+
+if __name__ == '__main__':
+    main()
