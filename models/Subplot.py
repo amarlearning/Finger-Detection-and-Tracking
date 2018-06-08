a4a828a15b3c9b4dadbcfd1c44b2f361f0a9abfd
Amar Prakash Pandey <amar.om1994@gmail.com>
2018-06-08 23:45:02 +0530

    matplotlib subplot func

---
+++ models/Subplot.py
@@ -0,0 +1,29 @@
+import cv2
+import matplotlib.pyplot as plt
+
+
+def main():
+
+    basePath = "/home/amarpandey/PycharmProjects/OpenCV/data/"
+
+    imageOneName = basePath + "4.1.01.tiff"
+    imageTwoName = basePath + "4.1.02.tiff"
+
+    imageOne = cv2.imread(imageOneName, 1)
+    imageTwo = cv2.imread(imageTwoName, 1)
+
+    imageNames = [imageOne, imageTwo]
+    imageTitles = ["First Image", "Second Image"]
+
+    for i in range(2):
+        plt.subplot(1, 2, i + 1)
+        plt.imshow(imageNames[i])
+        plt.title(imageTitles[i])
+        plt.xticks([])
+        plt.yticks([])
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
