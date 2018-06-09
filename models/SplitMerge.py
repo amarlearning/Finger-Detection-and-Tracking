c92b0359a066d2d67196576ab4c665fcc11c99e9
Amar Prakash Pandey <amar.om1994@gmail.com>
2018-06-09 20:03:19 +0530

    split merge in cv2

---
+++ models/SplitMerge.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+import cv2
+import matplotlib.pyplot as plt
+
+
+def main():
+    
+    basePath = "/home/amarpandey/PycharmProjects/OpenCV/data/"
+    imagePathOne = basePath + "house.tiff"
+    
+    imageOne = cv2.imread(imagePathOne, 1)
+    imageOne = cv2.cvtColor(imageOne, cv2.COLOR_BGR2RGB)
+    
+    red, green, blue = cv2.split(imageOne)
+    
+    images = [cv2.merge((red, green, blue)), red, green, blue]
+    titles = ["Default RGB Image", "Only Red", "Only Blue", "Only Green"]    
+    cmaps = ["gray", "Reds", "Greens", "Blues"]    
+    
+    
+    for i in range(4):    
+    
+        plt.subplot(2, 2, i + 1)
+    
+        plt.imshow(images[i], cmap = cmaps[i])
+        plt.title(titles[i])
+        plt.xticks([])        
+        plt.yticks([])
+        
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
