0e80ef37352cc9f6b0a4a3aebb4a568a9f674d5f
Amar Prakash Pandey <amar.om1994@gmail.com>
2018-06-13 23:46:56 +0530

    basic scaling operations

--- models/Scaling.py
+++ models/Scaling.py
@@ -2,8 +2,17 @@ import cv2
 
 
 def main():
-    pass
 
+    basePath = "../data/"
+    imageOneFile = basePath + "4.1.04.tiff"
+
+    imageOne = cv2.imread(imageOneFile, 1)
+
+    output = cv2.resize(imageOne, )
+
+    cv2.imshow("Resized Image", imageOne)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     main()
