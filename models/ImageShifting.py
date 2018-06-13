55b1e77820d94120a8e6ff7687596fe0183e8e3f
Amar Prakash Pandey <amar.om1994@gmail.com>
2018-06-14 01:25:31 +0530

    image shifting using warpAffine

---
+++ models/ImageShifting.py
@@ -0,0 +1,22 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def main():
+    imagePath = "../data/4.1.04.tiff"
+    shifting = np.float32([[1, 0, 50], [0, 1, 50]])
+
+    image = cv2.imread(imagePath, 1)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    rows, columns, channels = image.shape
+
+    shiftedImage = cv2.warpAffine(image, shifting, (rows, columns))
+
+    plt.imshow(shiftedImage)
+    plt.title("Shifted Image")
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
