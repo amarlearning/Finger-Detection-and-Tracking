65368ddadd64008c23d7c1fff9dfa68c30559cf8
Amar Prakash Pandey <amar.om1994@gmail.com>
2018-06-14 00:59:42 +0530

    some interpolation methods for resizing image

--- models/Scaling.py
+++ models/Scaling.py
@@ -2,17 +2,25 @@ import cv2
 
 
 def main():
+    imagePath = "../data/4.1.04.tiff"
 
-    basePath = "../data/"
-    imageOneFile = basePath + "4.1.04.tiff"
+    imageOne = cv2.imread(imagePath, 1)
 
-    imageOne = cv2.imread(imageOneFile, 1)
+    areaInter = cv2.resize(imageOne, None, fx=3, fy=3, interpolation=cv2.INTER_AREA)
+    cubicInter = cv2.resize(imageOne, None, fx=3, fy=3, interpolation=cv2.INTER_CUBIC)
+    linearInter = cv2.resize(imageOne, None, fx=3, fy=3, interpolation=cv2.INTER_LINEAR)
+    nearestInter = cv2.resize(imageOne, None, fx=3, fy=3, interpolation=cv2.INTER_NEAREST)
+    lancz0s4Inter = cv2.resize(imageOne, None, fx=3, fy=3, interpolation=cv2.INTER_LANCZOS4)
 
-    output = cv2.resize(imageOne, )
+    cv2.imshow("Area Interpolation Image", areaInter)
+    cv2.imshow("Cubic Interpolation Image", cubicInter)
+    cv2.imshow("Linear Interpolation Image", linearInter)
+    cv2.imshow("Nearest Interpolation Image", nearestInter)
+    cv2.imshow("LANCZ0S4 Interpolation Image", lancz0s4Inter)
 
-    cv2.imshow("Resized Image", imageOne)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
 
 if __name__ == '__main__':
     main()
