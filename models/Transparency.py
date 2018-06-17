fa740cbe802a3e65aa4d355fe1a047efa11e6a4e
Amar Prakash Pandey <amar.om1994@gmail.com>
2018-06-17 19:09:10 +0530

    pixel manupilation and filtering

---
+++ models/Transparency.py
@@ -0,0 +1,20 @@
+import cv2
+
+def main():
+    image = cv2.imread("../data/house.tiff", 1)
+
+    # Instead of split sue this way, more fast
+    blue = image[:, :, 0]
+    green = image[:, :, 1]
+    red = image[:, :, 2]
+
+    # combine each color spacing with alpha channel
+    rgba = cv2.merge((blue, green, red, red))
+
+    # cv2 gui does not support alpha value, so use default OS viewer
+    # use png format, because jpeg does not support alpha channel
+    print(cv2.imwrite("../data/rgba_red.png", rgba))
+
+
+if __name__ == '__main__':
+    main()
