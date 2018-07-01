8a423d8fe47571dfa9ae4a58fd7238365954a90e
Amar Prakash Pandey <amar.om1994@gmail.com>
2018-07-02 04:21:46 +0530

    contours methos area, perimeter, moments

--- models/Contours.py
+++ models/Contours.py
@@ -3,23 +3,36 @@ import numpy as  np
 
 
 def main():
-    kernal = np.ones((5, 5), np.uint8)
-    image = cv2.imread("../data/contour_sample.jpg", 1)
+    image = cv2.imread("../data/detect_blob.png", 1)
 
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    _, binay_thresh = cv2.threshold(gray_image, 90, 255, cv2.THRESH_BINARY)
-    cv2.imshow("Binary Thresholding Image", binay_thresh)
+    binay_thresh = cv2.adaptiveThreshold(gray_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 1)
 
-    '''
-    erode = cv2.erode(binay_thresh, kernal, iterations=1)
-    cv2.imshow("Eroded Image", erode)
-    '''
+    _, contours, hierarchy = cv2.findContours(binay_thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(image, contours, -1, (0, 255, 0), 3)
 
-    image_two, contours, hierarchy = cv2.findContours(binay_thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(image, contours, 55, (0, 255, 0), 3)
+    new_image = np.zeros((image.shape[0], image.shape[1], 3), np.uint8)
 
-    cv2.imshow("Contoured Image", image)
+    for cnt in contours:
+        cv2.drawContours(new_image, [cnt], -1, (255, 0, 255), -1)
+
+        # get contour area using 'contourArea' method
+        area_cnt = cv2.contourArea(cnt)
+
+        # get the perimeter of any contour using 'arcLength'
+        perimeter_cnt = cv2.arcLength(cnt, True)
+
+        # get centroid oy contour using moments
+        M = cv2.moments(cnt)
+        cx = int(M['m10'] / M['m00'])
+        cy = int(M['m01'] / M['m00'])
+
+        cv2.circle(new_image, (cx, cy), 3, (0, 255, 0), -1)
+
+        print("Area : {}, Perimeter : {}".format(area_cnt, perimeter_cnt))
+
+    cv2.imshow("Contoured Image", new_image)
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
