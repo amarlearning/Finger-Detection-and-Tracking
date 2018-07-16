f442bd38787beeab2e4592d6916591092d6861fb
Amar Prakash Pandey <amar.om1994@gmail.com>
2018-07-17 03:15:01 +0530

    hough line detection using opencv

--- models/HoughLine.py
+++ models/HoughLine.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 
+
 def main():
     capture = cv2.VideoCapture(0)
 
@@ -9,9 +10,20 @@ def main():
 
         gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        edges_detec = cv2.Canny(gray_frame, 100, 200, apertureSize=3, L2gradient=True)
+        edges_detec = cv2.Canny(gray_frame, 50, 250, apertureSize=3, L2gradient=True)
 
-        cv2.imshow("Capture Frame", edges_detec)
+        hough_lines = cv2.HoughLines(edges_detec, 1, np.pi / 180, 200)
+
+        if hough_lines is not None:
+            for rho, theta in hough_lines[0]:
+                x0 = rho * np.cos(theta)
+                y0 = rho * np.sin(theta)
+
+                ptsX = (int(x0 + 1000 * (-np.sin(theta))), int(y0 + 1000 * (np.cos(theta))))
+                ptsY = (int(x0 - 1000 * (-np.sin(theta))), int(y0 - 1000 * (np.cos(theta))))
+                cv2.line(frame, ptsX, ptsY, (0, 255, 0), 2)
+
+        cv2.imshow("Capture Frame", frame)
 
         if cv2.waitKey(1) == 27:
             break
