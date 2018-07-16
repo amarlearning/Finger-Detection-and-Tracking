b4e80a79f40455a117c0b4a15f37c2e06c830ac4
Amar Prakash Pandey <amar.om1994@gmail.com>
2018-07-16 23:28:38 +0530

    unwanted comment removed

--- models/HoughLine.py
+++ models/HoughLine.py
@@ -9,7 +9,6 @@ def main():
 
         gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        # edges_detec = cv2.Canny(gray_frame, 100, 200, apertureSize=3)
         edges_detec = cv2.Canny(gray_frame, 100, 200, apertureSize=3, L2gradient=True)
 
         cv2.imshow("Capture Frame", edges_detec)
