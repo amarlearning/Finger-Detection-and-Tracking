63a6b9517c3ff52c8cd155558f343fea2448e521
Amar Prakash Pandey <amar.om1994@gmail.com>
2018-07-18 04:23:53 +0530

    add eye classifier to detect eyes

--- models/Haarcascade.py
+++ models/Haarcascade.py
@@ -3,18 +3,25 @@ import cv2
 
 def main():
     capture = cv2.VideoCapture(0)
-    path = "../classifier/haarcascade_frontalface_default.xml"
+    eye_path = "../classifier/haarcascade_eye.xml"
+    face_path = "../classifier/haarcascade_frontalface_default.xml"
 
-    face_cascade = cv2.CascadeClassifier(path)
+    eye_cascade = cv2.CascadeClassifier(eye_path)
+    face_cascade = cv2.CascadeClassifier(face_path)
 
     while (True):
         _, frame = capture.read()
 
         gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        faces = face_cascade.detectMultiScale(gray_frame, scaleFactor=1.05, minNeighbors=5, minSize=(40, 40))
+        eyes = eye_cascade.detectMultiScale(gray_frame, scaleFactor=1.10, minNeighbors=5, minSize=(10,10))
+        faces = face_cascade.detectMultiScale(gray_frame, scaleFactor=1.15, minNeighbors=5, minSize=(40, 40))
 
+        print("Number of eyes : " + str(len(eyes)))
         print("Number of faces : " + str(len(faces)))
+
+        for (x, y, w, h) in eyes:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
         for (x, y, w, h) in faces:
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
