faf2021f9c30ecb4beae0b9c54bb6d89f98acff2
Amar Prakash Pandey <amar.om1994@gmail.com>
2018-07-22 18:22:41 +0530

    hand detected

--- models/FingerDetection.py
+++ models/FingerDetection.py
@@ -1,6 +1,14 @@
 import cv2
 import numpy as np
 
+hand_hist = None
+total_rectangle = 9
+hand_rect_one_x = None
+hand_rect_one_y = None
+
+hand_rect_two_x = None
+hand_rect_two_y = None
+
 
 def rescale_frame(frame, wpercent=100, hpercent=100):
     width = int(frame.shape[1] * wpercent / 100)
@@ -8,8 +16,32 @@ def rescale_frame(frame, wpercent=100, hpercent=100):
     return cv2.resize(frame, (width, height), interpolation=cv2.INTER_AREA)
 
 
+def contours(hist_mask_image):
+    gray_hist_mask_image = cv2.cvtColor(hist_mask_image, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(gray_hist_mask_image, 0, 255, 0)
+    _, cont, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    return cont
+
+
+def max_contour(contour_list):
+    max_i = 0
+    max_area = 0
+
+    for i in range(len(contour_list)):
+        cnt = contour_list[i]
+
+        area_cnt = cv2.contourArea(cnt)
+
+        if area_cnt > max_area:
+            max_area = area_cnt
+            max_i = i
+
+        return contour_list[max_i]
+
+
 def draw_rect(frame):
     rows, cols, _ = frame.shape
+    global total_rectangle, hand_rect_one_x, hand_rect_one_y, hand_rect_two_x, hand_rect_two_y
 
     hand_rect_one_x = np.array(
         [6 * rows / 20, 6 * rows / 20, 6 * rows / 20, 9 * rows / 20, 9 * rows / 20, 9 * rows / 20, 12 * rows / 20,
@@ -19,30 +51,90 @@ def draw_rect(frame):
         [9 * cols / 20, 10 * cols / 20, 11 * cols / 20, 9 * cols / 20, 10 * cols / 20, 11 * cols / 20, 9 * cols / 20,
          10 * cols / 20, 11 * cols / 20], dtype=np.uint32)
 
-    hand_rect_two_x = hand_rect_one_x + 20
-    hand_rect_two_y = hand_rect_one_y + 20
+    hand_rect_two_x = hand_rect_one_x + 10
+    hand_rect_two_y = hand_rect_one_y + 10
 
-    for i in range(hand_rect_one_x.size):
+    for i in range(total_rectangle):
         cv2.rectangle(frame, (hand_rect_one_y[i], hand_rect_one_x[i]),
                       (hand_rect_two_y[i], hand_rect_two_x[i]),
-                      (0, 255, 0), 2)
+                      (0, 255, 0), 1)
 
-    black_frame = np.zeros(frame.shape, dtype=frame.dtype)
-    return np.vstack([frame, black_frame])
+    return frame
+
+
+def hand_histogram(frame):
+    global hand_rect_one_x, hand_rect_one_y
+
+    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    roi = np.zeros([90, 10, 3], dtype=hsv_frame.dtype)
+
+    for i in range(total_rectangle):
+        roi[i * 10: i * 10 + 10, 0: 10] = hsv_frame[hand_rect_one_x[i]:hand_rect_one_x[i] + 10,
+                                          hand_rect_one_y[i]:hand_rect_one_y[i] + 10]
+
+    hand_hist = cv2.calcHist([roi], [0, 1], None, [180, 256], [0, 180, 0, 256])
+    return cv2.normalize(hand_hist, hand_hist, 0, 255, cv2.NORM_MINMAX)
+
+
+def hist_masking(frame, hist):
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    dst = cv2.calcBackProject([hsv], [0, 1], hist, [0, 180, 0, 256], 1)
+
+    disc = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (31, 31))
+    cv2.filter2D(dst, -1, disc, dst)
+
+    ret, thresh = cv2.threshold(dst, 150, 255, cv2.THRESH_BINARY)
+
+    cv2.imshow("normal thresh", thresh)
+
+    thresh = cv2.merge((thresh, thresh, thresh))
+
+    cv2.GaussianBlur(dst, (5, 5), 0, dst)
+
+    return cv2.bitwise_and(frame, thresh)
+
+
+def manage_image_opr(frame, hand_hist):
+    hist_mask_image = hist_masking(frame, hand_hist)
+    contour_list = contours(hist_mask_image)
+    max_cont = max_contour(contour_list)
+    hull = cv2.convexHull(max_cont, returnPoints=False)
+    defects = cv2.convexityDefects(max_cont, hull)
+
+    if defects is not None:
+        for i in range(defects.shape[0]):
+            s, e, f, d = defects[i, 0]
+            start = tuple(max_cont[s][0])
+            end = tuple(max_cont[e][0])
+            far = tuple(max_cont[f][0])
+            cv2.line(frame, start, end, [0, 255, 0], 2)
+            cv2.circle(frame, far, 5, [0, 0, 255], -1)
 
 
 def main():
+    global hand_hist
+    is_hand_hist_created = False
     capture = cv2.VideoCapture(0)
 
     while capture.isOpened():
+        pressed_key = cv2.waitKey(1)
         _, frame = capture.read()
 
         frame = rescale_frame(frame)
-        frame = draw_rect(frame)
+
+        if pressed_key & 0xFF == ord('z'):
+            is_hand_hist_created = True
+            hand_hist = hand_histogram(frame)
+
+        if is_hand_hist_created:
+            manage_image_opr(frame, hand_hist)
+
+        else:
+            frame = draw_rect(frame)
 
         cv2.imshow("Live Feed", frame)
 
-        if cv2.waitKey(1) == 27:
+        if pressed_key == 27:
             break
 
     cv2.destroyAllWindows()
